3c19cd5938d59185aefa2cca24abd75306935dfa
CodyBeyer <codybeyer@Codys-MacBook-Pro.local>
2014-09-14 13:39:02 -0700

    First Commit

---
+++ Entities/ForecastIORequest.cs
@@ -0,0 +1,76 @@
+﻿using ForecastIO.Extensions;
+using System;
+using System.Globalization;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+
+namespace ForecastIO
+{
+    public class ForecastIORequest
+    {
+        private readonly string _apiKey;
+        private readonly string _latitude;
+        private readonly string _longitude;
+        private readonly string _unit;
+        private readonly string _exclude;
+        private readonly string _extend;
+        private readonly string _time;
+        //
+
+        public string url = "";
+        public string result = "";
+        private const string CurrentForecastUrl = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&extend={4}&exclude={5}";
+        private const string PeriodForecastUrl = "https://api.forecast.io/forecast/{0}/{1},{2},{3}?units={4}&extend={5}&exclude={6}";
+
+        public async Task<ForecastIOResponse> Get()
+        {
+
+            url = (_time == null) ? String.Format(CurrentForecastUrl, _apiKey, _latitude, _longitude, _unit, _extend, _exclude) :
+            String.Format(PeriodForecastUrl, _apiKey, _latitude, _longitude, _time, _unit, _extend, _exclude);
+
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            using (HttpContent content = response.Content)
+            {
+                result = await content.ReadAsStringAsync();
+            }
+            ForecastIOResponse returnObj = Newtonsoft.Json.JsonConvert.DeserializeObject<ForecastIOResponse>(result);
+
+
+            return returnObj;
+        }
+
+
+        //public async Task<ForecastIOResponse> getJson(string uri)
+        //{
+        //    var httpClient = new HttpClient();
+        //    var content = await httpClient.GetStringAsync(uri);
+        //    return await Task.Run(() => JsonConvert.DeserializeObject<ForecastIOResponse>(content));
+        //}
+
+        public ForecastIORequest(string apiKey, float latF, float longF, Unit unit, Extend[] extend = null, Exclude[] exclude = null)
+        {
+            _apiKey = apiKey;
+            _latitude = latF.ToString(CultureInfo.InvariantCulture);
+            _longitude = longF.ToString(CultureInfo.InvariantCulture);
+            _unit = Enum.GetName(typeof(Unit), unit);
+            _extend = (extend != null) ? RequestHelpers.FormatExtendString(extend) : "";
+            _exclude = (exclude != null) ? RequestHelpers.FormatExcludeString(exclude) : "";
+        }
+
+        public ForecastIORequest(string apiKey, float latF, float longF, DateTime time, Unit unit, Extend[] extend = null, Exclude[] exclude = null)
+        {
+            _apiKey = apiKey;
+            _latitude = latF.ToString(CultureInfo.InvariantCulture);
+            _longitude = longF.ToString(CultureInfo.InvariantCulture);
+            _time = time.ToUTCString();
+            _unit = Enum.GetName(typeof(Unit), unit);
+            _extend = (extend != null) ? RequestHelpers.FormatExtendString(extend) : "";
+            _exclude = (exclude != null) ? RequestHelpers.FormatExcludeString(exclude) : "";
+        }
+
+    }
+}
